0022ea70b5a55e0a392e5df7511ef585afa77d3d
Opilane TTHK <opilane@tthk.ee>
2020-11-18 15:01:23 +0200

    first commit

---
+++ CreateDirectory/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace CreateDirectory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        public static void CreateFolderOne()
+        {
+            string folderootPath = @"C:\Users\opilane\samples";
+            string FolderName = "playOne";
+            string fullFolderPath = Path.Combine(folderootPath, FolderName);
+            Console.WriteLine(fullFolderPath);
+
+        }
+    }
+}
